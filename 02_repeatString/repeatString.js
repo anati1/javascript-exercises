d9499744b493c1b62fb415b0e3d4605c7bf07b3d
Anat <anat_ilani@walla.co.il>
2022-07-26 23:00:28 +0300

    Added solution to exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    result = '';
+    if (times < 0)
+    return'ERROR';
+    for (let i = 0; i < times; i++){
+        result = result.concat(str);
+    }
+    return result;
 };
 
 // Do not edit below this line
