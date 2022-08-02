87e2568547fec7b27c3af7dce34872effba127ac
Anat <anat_ilani@walla.co.il>
2022-08-02 22:40:45 +0300

    Added solution to exercises 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(myStr) {
+    myArr = myStr.split('');
+    let temp
+    //Switch the first half with last half of array
+    for(let i = 0, j = myArr.length-1; i<=j ;i++, j-- ){
+        temp = myArr[i];
+        myArr[i] = myArr[j];
+        myArr[j] = temp;
+    }
+    return myArr.join('');
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArr,...theArgs) {
+    let lastIndex = myArr.length;
+    let lastIndexOfArgs = theArgs.length;
+    for(let i = 0; i < lastIndex; i++){
+        for(let j = 0; j < lastIndexOfArgs; j++){
+            if (myArr[i] === theArgs[j]){
+                myArr.splice(i,1);
+                i--;
+                lastIndex = myArr.length;
+            }
+        }
+    }
+    return myArr;
 };
 
 // Do not edit below this line
