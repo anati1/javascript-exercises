a4606016bf89200151b1e0eb3cdce64157e5971b
Anat <anat_ilani@walla.co.il>
2022-08-05 20:23:40 +0300

    Added solution to exercises 05, 06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let ctemp = ((ftemp - 32)*(5.0/9.0));
+  if (Number.isInteger(ctemp))
+      return ctemp;
+  else
+     return parseFloat(ctemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  ftemp = (ctemp*(9.0/5.0)) + 32;
+  if (Number.isInteger(ftemp))
+      return ftemp;
+  else
+     return parseFloat(ftemp.toFixed(1));
 };
 
 // Do not edit below this line
