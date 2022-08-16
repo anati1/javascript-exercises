20cdc72914f56434c0c4cac5b790b0c2f8daa618
Anat <anat_ilani@walla.co.il>
2022-08-16 22:07:34 +0300

    Added solution to exercises 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let checkPalindrome = str.replace(/[!,\.\s]/g, "").toLowerCase();
+    let result = checkPalindrome.toLowerCase().split("").reverse().join("");
+    return checkPalindrome === result ? true : false;
 };
 
 // Do not edit below this line
