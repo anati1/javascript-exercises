591cbefae24d7cfa5df69a1b2f539b8fb59a8b65
Anat <anat_ilani@walla.co.il>
2022-08-19 15:32:03 +0300

    Added solution to exercises 10, 11 and 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number.parseInt(num);
+    if (Number.isNaN(num) || num < 1){
+        return "OOPS"
+    }
+    if (num === 1 ||  num === 2)
+        return 1; //first and second fibonacci sequence numbers
+    else
+    {
+        first = 1;
+        second = 1;
+        current = 1;
+        for(let i = 3; i <= num ; i++){
+            current = first + second;
+            first = second;
+            second = current;
+        }
+    }
+    return current;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   return people.reduce(function(oldest, person) {
+       //In case there no yearOfDeath
+       let oldestYearOfDeath = oldest.yearOfDeath === undefined ? (new Date()).getFullYear() : oldest.yearOfDeath;
+       let personYearOfDeath = person.yearOfDeath === undefined ? (new Date()).getFullYear() : person.yearOfDeath;
+       //Calculate age
+       let oldestPersonAge = oldestYearOfDeath - oldest.yearOfBirth; 
+       let currentPersonAge = personYearOfDeath - person.yearOfBirth;
+       //Check who is the oldest
+       if (currentPersonAge > oldestPersonAge)
+           return person;
+       else
+           return oldest;
+    },
+       {
+            name: "Name",
+            yearOfBirth: 0,
+            yearOfDeath: 0
+       }
+    );
 };
 
 // Do not edit below this line
